632f7510474a278cb6145179872eedfd6c7039f7
Voloshyn9900 <voloshyn9900@gmail.com>
2025-05-20 17:07:48 +0300

    Start hw3-crud task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+// dotenv.config();
 
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
